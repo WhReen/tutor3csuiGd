f8dc26c4cec608da715e8f7d16c0ed98b810015b
WhReen <horrofer@gmail.com>
2024-03-07 12:33:41 +0700

    implement tutorial 5 basic and some assignment implementation

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Tambahan mekanik peregerakan:
 >Crouch: Menekan arah bawah melambatkan pergerakan x.axis dan juga mempercepat gerakan gravitasi jika tidak on_ground
 >Glide: Menahan input gerakan atas di atas tanah membatasi gerakan gravitasi sebanyak 1/16 dari gravitasi
 >SuperJump: akan lompat sangat tinggi jika ketika release input bawah langsung input secara Frame Perfect
+
+------TUTORIAL 5---------
+>Menambah custom bgm
+
+>SFX ketika melakukan superJump
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,7 +7,9 @@ export (int) var doubletapdelay= 0.25
 export (int) var stopTime = 0.1
 export (int) var jumpAmount = 2
 
-onready var sprite = $Sprite
+onready var sprite = $AnimatedSprite
+onready var collision = $CollisionShape2D
+onready var sfxplayer = $AudioStreamPlayer2D
 
 var doubletaptime = 0
 var dtappedright = false
@@ -22,27 +24,36 @@ const UP = Vector2(0,-1)
 var velocity = Vector2()
 
 
-
-
 func sprite(): #sprite manipulation
 	sprite.rotation_degrees = 90*(velocity.x/(realSpeed*4))
+	sprite.flip_h = velocity.x < 0
+	
 	
 
 func get_input(delta):
+	var animation = "diri_kanan"
+	
 	if crouch: #crouch movement speed
 		speed = realSpeed/2
 	else:
 		speed = realSpeed
 		
-	if Input.is_action_just_released('ui_down'):
-		superjumpTime = delta
-		
-	if Input.is_action_just_pressed('ui_up') and Input.is_action_pressed('ui_down') and is_on_floor():
-		velocity.y = -jumpSpeed
+	if jumpLeft > 0 and Input.is_action_just_pressed('ui_up'):
+		if superjumpTime > 0:
+			velocity.y = -jumpSpeed*2
+			sprite.scale.x = 0.3
+			sfxplayer.play()
+		else:
+			velocity.y = -jumpSpeed
+			sprite.scale.x = 0.5
 		jumpLeft -= 1
+	
+	if Input.is_action_just_released('ui_down'):
+		superjumpTime = delta*5
 		
 		
 	if Input.is_action_just_pressed('ui_right'): #doubletap right to run right
+		animation = "jalan_kanan"
 		if doubletaptime > 0 and velocity.x > 0:
 			dtappedright = true
 			if velocity.y > 0:
@@ -81,15 +92,6 @@ func get_input(delta):
 	if Input.is_action_just_released('ui_left'): #limiting dash
 		if dtappedleft:
 			dtappedleft = false
-		
-	if jumpLeft > 0 and Input.is_action_just_pressed('ui_up'):
-		if superjumpTime > 0:
-			velocity.y = -jumpSpeed*2
-			sprite.scale.x = 0.3
-		else:
-			velocity.y = -jumpSpeed
-			sprite.scale.x = 0.5
-		jumpLeft -= 1
 	
 	if Input.is_action_pressed('ui_up'):
 		if sprite.scale.x > 0.7:
@@ -100,24 +102,31 @@ func get_input(delta):
 	if Input.is_action_pressed('ui_down'): #crouch and faster down movement
 		velocity.y += speed
 		sprite.scale.y = 0.5
+		collision.scale.y = 0.5
 		crouch = true
 	else:
 		sprite.scale.y = 1
+		collision.scale.y = 1
 		crouch = false
+		
+	if $AnimatedSprite.animation != animation:
+		$AnimatedSprite.play(animation)
 
 func _physics_process(delta):
 	if doubletaptime > 0:
 		doubletaptime -= delta
 	if is_on_floor():
 		jumpLeft = jumpAmount
+	if superjumpTime > 0:
+		superjumpTime-=delta
+		print_debug(superjumpTime)
 	velocity.y += delta * GRAVITY
 	get_input(delta)
 	sprite()
-	superjumpTime -= delta
 	velocity = move_and_slide(velocity, UP)
 	var slowdown := min(delta/stopTime, 1.0) #x axis slowdown when no x axis movement input pressed
 	velocity.x -= velocity.x * slowdown
 	var slowdownSprite := min(delta/1.6, 1.0)
 	if sprite.scale.x < 1:
 		sprite.scale.x += sprite.scale.x * slowdown
-	print_debug(sprite.scale.x)
+	
